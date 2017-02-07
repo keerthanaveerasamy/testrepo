2b1bd722391cde789e6c0f6aa2a15d0664642c29
ITLAB <ITLAB@IT206>
2017-02-07 16:01:28 +0530

    commit4

---
+++ src/SecondActivity.java
@@ -0,0 +1,98 @@
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class SecondActivity extends Activity
+{
+	String data,d;
+	int dim,dim1;
+	 GraphicsView graphicsview;  
+	     
+	protected void onCreate(Bundle  savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.scndactivity);
+        graphicsview = new GraphicsView(this);  
+        setContentView(graphicsview);  
+ 
+
+		//Intent i=getIntent();
+		 //data= i.getStringExtra("value");
+	}
+	private class GraphicsView extends View{  
+        public GraphicsView(Context context){  
+            super(context);  
+        }  		
+
+				@Override
+				protected void onDraw(Canvas canvas) {
+					super.onDraw(canvas);
+					Intent i=getIntent();
+					data= i.getStringExtra("value");
+					d=i.getStringExtra("dimension");
+					dim=Integer.parseInt(d);
+					dim1=dim*2;
+					// custom drawing code here
+					Paint paint = new Paint();
+					paint.setStyle(Paint.Style.FILL);
+		 
+					// make the entire canvas white
+					paint.setColor(Color.CYAN);
+					canvas.drawPaint(paint);
+			
+					if(data.equals("rectangle")){
+					paint.setAntiAlias(false);
+					paint.setColor(Color.MAGENTA);
+					canvas.drawRect(10, 10,dim,dim1, paint);
+					}
+		 
+				
+					if(data.equals("circle")){
+					paint.setAntiAlias(true);
+					paint.setColor(Color.GRAY);
+					canvas.drawCircle(60, 20, dim, paint);
+					}
+		 
+					if(data.equals("square")){
+					paint.setAntiAlias(false);
+					paint.setColor(Color.RED);
+					canvas.drawRect(100, 5, dim, dim, paint);
+					}
+		 
+					canvas.rotate(-40);
+					if(data.equals("string")){
+		 
+					paint.setStyle(Paint.Style.FILL);
+					canvas.drawText("Graphics Rotation", 45, 185, paint);
+					}
+		 
+					// undo the rotate
+					canvas.restore();
+				}
+			}
+	 @Override  
+	    public boolean onCreateOptionsMenu(Menu menu) {  
+	        // Inflate the menu; this adds items to the action bar if it is present.  
+	        getMenuInflater().inflate(R.menu.main, menu);  
+	        return true;  
+	    }  
+	}
+		
+	
+
+
+    
+
+
--- src/com/example/testpro/MainActivity.java
+++ src/com/example/testpro/MainActivity.java
@@ -1,35 +1,89 @@
-package com.example.testpro;
+package com.example.TestPro;
 
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;  
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+	Button rect,sq,cir,str;
+	TextView text;
+	EditText ed1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        rect=(Button) findViewById(R.id.rect);
+        sq=(Button) findViewById(R.id.sq);
+        cir=(Button) findViewById(R.id.cir);
+        str=(Button) findViewById(R.id.str);
+        text=(TextView) findViewById(R.id.text);
+        ed1=(EditText) findViewById(R.id.ed1);
+        
+        
+        rect.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				String data="rectangle";
+				String val=ed1.getText().toString();
+				Intent i=new Intent(MainActivity.this,SecondActivity.class);
+				i.putExtra("value", data);
+				i.putExtra("dim", data);
+				startActivity(i);
+			}
+		});
+        sq.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				String val=ed1.getText().toString();
+				String data="square";
+				Intent i=new Intent(MainActivity.this,SecondActivity.class);
+				i.putExtra("value", data);
+				i.putExtra("dim", data);
+				startActivity(i);
+			}
+		});
+        cir.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			String val=ed1.getText().toString();
+			String data="circle";
+			Intent i=new Intent(MainActivity.this,SecondActivity.class);
+			i.putExtra("value", data);
+			i.putExtra("dim", data);
+			startActivity(i);
+		}
+	});
+        str.setOnClickListener(new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View arg0) {
+			String val=ed1.getText().toString();
+			String data="string";
+			Intent i=new Intent(MainActivity.this,SecondActivity.class);
+			i.putExtra("value", data);
+			i.putExtra("dim", data);
+			startActivity(i);
+		}
+	});
+    
+    
+    
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
+   
